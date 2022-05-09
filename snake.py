71f67fcc389d00287b152d4d04919fc672e7be6e
Kartik Kumar <61470841+iamKaartik@users.noreply.github.com>
2022-05-09 07:21:46 +0530

    Updating snake.py file

--- snake.py
+++ snake.py
@@ -14,17 +14,23 @@ class Snake:
 
     def create_snake(self):
         for pos in POSITION:
-            new_segment = Turtle("square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(pos)
-            self.segments.append(new_segment)
+            self.add_segment(pos)
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
             self.segments[seg_num].goto(self.segments[seg_num - 1].xcor(), self.segments[seg_num - 1].ycor())
 
         self.segments[0].forward(MOVE)
+
+    def add_segment(self, pos):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(pos)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
 
     def up(self):
         if self.segments[0].heading() != DOWN:
